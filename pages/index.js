c12b7d64415fe6ac61cccf29419b259e3a999755
leonfilho <leonfilho@gmail.com>
2024-11-19 15:31:14 +0000

    Incluida a pasta de paginas

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <div>
+        <h1>Ao meu amor serei atento...</h1>
+        <p>...E cada verso meu será pra te dizer 
+            <br/>que eu sei que vou te amar, <br/>por toda minha vida...</p></div>
+}
+
+export default Home;
