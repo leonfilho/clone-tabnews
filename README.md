7e85921c2a3d008295cdf831d85417ee89e0fca0
leonfilho <leonfilho@gmail.com>
2024-12-03 21:04:11 +0000

    Configuração `prettier`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do projeto TabNews do curso .dev para exercitar habilidades em programação full-stack.
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 function Home() {
-    return <div>
-        <h1>Ao meu amor serei atento...</h1>
-        <p>...E cada verso meu será pra te dizer 
-            <br/>que eu sei que vou te amar, 
-            <br/>por toda minha vida...!</p></div>
+  return (
+    <div>
+      <h1>Ao meu amor serei atento...</h1>
+      <p>
+        ...E cada verso meu será pra te dizer
+        <br />
+        que eu sei que vou te amar,
+        <br />
+        por toda minha vida...!
+      </p>
+    </div>
+  );
 }
 
 export default Home;
