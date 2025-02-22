010d3ebd32b9046b7918962e687812b8eefa0141
leonfilho <leonfilho@gmail.com>
2025-02-22 23:44:54 +0000

    adicionado arquivo de configuracao `compose yaml` para imagem docker do postgres

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,5 @@
+// levantar o servidor de desenvolvimento
+// >npm run dev
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   //console.log(response);
