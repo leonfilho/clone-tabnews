6184e93727933ee8d1485265290dac0320e2ff09
leonfilho <leonfilho@gmail.com>
2025-03-08 14:40:09 +0000

    Incluindo os arquivos `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,12 @@
-function status(request, response) {
-  response.status(200).json({ chave: "É a resposta da api na raça!" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  console.log("Iniciando o status...");
+  const result = await database.query("SELECT 1+1 Resultado;");
+  console.log(result.rows);
+  console.log(result.rows[0]);
+  console.log(result.rows[0].resultado);
+  response.status(200).json({ chave: "Alteração do valor retornado" });
 }
 
 export default status;
