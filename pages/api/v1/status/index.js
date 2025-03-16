fba247d7b4cbe49cba9353843298f3c819008f3f
leonfilho <leonfilho@gmail.com>
2025-03-16 15:41:00 +0000

    Criada a propriedade de status `updated_at`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,10 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  console.log("Iniciando o status...");
-  const result = await database.query("SELECT 1+1 Resultado;");
-  console.log(result.rows);
-  console.log(result.rows[0]);
-  console.log(result.rows[0].resultado);
-  response.status(200).json({ chave: "Alteração do valor retornado" });
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,16 @@
 // levantar o servidor de desenvolvimento
 // >npm run dev
 test("Get to /api/v1/status should return 200", async () => {
+  //Testa se retornou codigo de sucesso
   const response = await fetch("http://localhost:3000/api/v1/status");
-  console.log(response);
   expect(response.status).toBe(200);
+
+  //Testa se a propriedade existe
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  //Testa se o valor da propriedade pode ser convertido numa data válida
+  //  e se a data convertida é a mesma que existe na propriedade
+  const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdateAt);
 });
