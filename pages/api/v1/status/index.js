2a3e537558ce0c834800ce4fdc0a4026d60eb4ce
leonfilho <leonfilho@gmail.com>
2025-03-17 02:31:41 +0000

    Finaliza o endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,46 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const dabaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue = Number(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  //Para testar o exemplo de SQL injection através de parametro (?databaseName=) da requisição
+  //const databaseName = request.query.databaseName;
+  //console.log(`O nome do banco de dados selecionado é '${databaseName}'`);
+  //usar a consulta "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';", (solução 1)
+
+  // Obtem o nome do banco das variáveis de ambiente
+  const databaseName = process.env.POSTGRES_DB;
+  // Busca na view de estat. de atividade do DBS, as conexões com o banco da aplicação
+  // usando o formato de Parameterized Query para evitar ataques de SQL Injection
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  // A quantidade de linhas no resultado da solução 1
+  // é a quantidade de sessões abertas. Nessa segunda solução, é a coluna count.
+  // Como o typecast da coluna foi feito direto no select, não precisa de parseInt()
+  const databaseOpenedConnectionsValue =
+    //databaseOpenedConnectionsResult.rows.length, (solução 1)
+    databaseOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dabaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,16 +1,44 @@
 // levantar o servidor de desenvolvimento
-// >npm run dev
+// $npm run dev
+
 test("Get to /api/v1/status should return 200", async () => {
   //Testa se retornou codigo de sucesso
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
-  //Testa se a propriedade existe
+  //Separa o corpo da resposta para facilitar o teste dos elementos
   const responseBody = await response.json();
+  //Testa se a propriedade updated_at existe
   expect(responseBody.updated_at).toBeDefined();
 
   //Testa se o valor da propriedade pode ser convertido numa data válida
   //  e se a data convertida é a mesma que existe na propriedade
   const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdateAt);
+
+  //Testa se a propriedade database_version existe
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  //Testa a versão do servidor DB - atual 16.0
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  //Testa se a propriedade database_max_conn existe
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  //Testa a quantidade máxima esperada de conn - default 100
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  //Testa se a propriedade database_used_conn existe
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+  //Testa a quantidade de conexões abertas - deve ser 1, pois nesse ponto
+  // dentro do ambiente dev, só abrimos uma conn por vez. Se abrir mais,
+  // tem conn vazando
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
+
+/* Teste exemplo temporário de SQL Injection
+test.only("Teste de SQL Injection", async () => {
+  //Chama o teste com injeção de SQL - faz uma espera de 4 segundos
+  const response = await fetch(
+    "http://localhost:3000/api/v1/status?databaseName=';select pg_sleep(4);--",
+  );
+});
+*/
